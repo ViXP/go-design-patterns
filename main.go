54820ef07b5fa636e7a6217b3397db489c4fe818
Cyril ViXP <cyrilvixp@gmail.com>
2025-05-13 13:42:29 +0300

    Move implemented patterns to GIT submodule

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "example.com/design-patterns/facade"
+
+func main() {
+	//specification.Run()
+	//builder.Run()
+	//factory.Run()
+	//prototype.Run()
+	//singleton.Run()
+	//adapter.Run()
+	//bridge.Run()
+	//composite.Run()
+	//decorator.Run()
+	facade.Run()
+}
